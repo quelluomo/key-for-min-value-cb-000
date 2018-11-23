3952d9c7809901f0d3714c9513b009da76aee44b
Matteo Muscas <thatman.j@gmail.com>
2018-11-23 10:57:41 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,8 +3,7 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-    value.map > nil
+    value.find key[value] < key[value]
       return key
-  lower = name_hash.find {|key, value| key[value] < key[:value] }  
   end
 end
